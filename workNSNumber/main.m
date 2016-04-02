d7d3c8778fe63d881e8dc3377eef5fd9d82060ab
Chang Ming-Han <minghan@ming-hantekimacbook-pro.local>
2016-04-02 16:42:26 +0800

    Initial Commit

---
+++ workNSNumber/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  workNSNumber
+//
+//  Created by Ming-Han Chang on 2016/4/2.
+//  Copyright © 2016年 Ming-Han Chang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
